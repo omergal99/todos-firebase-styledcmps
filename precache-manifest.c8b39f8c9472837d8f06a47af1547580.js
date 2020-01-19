0c946eec6e170e0a3b8c8007dd9d7f76ccb8e36e
omergal99 <omergal99gm@gmail.com>
2020-01-19 05:02:57 +0200

    Updates

---
+++ precache-manifest.c8b39f8c9472837d8f06a47af1547580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca50cb9ed10c74a85710629e3fba81f",
+    "url": "/todos-firebase-styledcmps/index.html"
+  },
+  {
+    "revision": "aa8194e004c97d7af47f",
+    "url": "/todos-firebase-styledcmps/static/css/main.943c3884.chunk.css"
+  },
+  {
+    "revision": "0b277bdfd33404b6308c",
+    "url": "/todos-firebase-styledcmps/static/js/2.09dc9653.chunk.js"
+  },
+  {
+    "revision": "7a014442becae4b69ee4fe88d1328bfe",
+    "url": "/todos-firebase-styledcmps/static/js/2.09dc9653.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa8194e004c97d7af47f",
+    "url": "/todos-firebase-styledcmps/static/js/main.7cabd792.chunk.js"
+  },
+  {
+    "revision": "6da4869ddf8350b9dff9",
+    "url": "/todos-firebase-styledcmps/static/js/runtime-main.4a9989e4.js"
+  }
+]);
